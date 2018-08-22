a075bc78d3efcda19af060fa6d940992bfb2862d
elim322 <einer.lim322@gmail.com>
2018-08-22 09:54:48 -0700

    updated js file and styled page submit logged in

--- js/api.js
+++ js/api.js
@@ -5,9 +5,17 @@
    * Ajax-based random post fetching & History API
    */
 
+lastPage = document.URL
+history.pushState(null, null, getRandomIndex());
 
   /**
    * Ajax-based front-end post submissions.
    */
 
 })(jQuery);
+
+
+
+$(window).on("popstate", function () {
+  window.location.replace(lastPage);
+});
